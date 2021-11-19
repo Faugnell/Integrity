ce734002d1eff0f46087694899b925442993e714
Faugnell <90843661+Faugnell@users.noreply.github.com>
2021-11-19 14:22:51 +0100

    Simplification du code

--- button.ino
+++ button.ino
@@ -1,33 +1,23 @@
-#define O0 11
-#define I0 A0
- 
-// constants won't change. They're used here to  
-// set pin numbers: 
-const int buttonPin = I0; // the number of the pushbutton pin 
-const int ledPin = O0; // the number of the LED pin 
- 
-// variables will change: 
-int buttonState = 0; // variable for reading the pushbutton status 
- 
-void setup() { 
-// initialize the LED pin as an output: 
-pinMode(ledPin, OUTPUT);  
-// initialize the pushbutton pin as an input: 
-pinMode(buttonPin, INPUT);  
-} 
- 
-void loop(){ 
-// read the state of the pushbutton value: 
-buttonState = digitalRead(buttonPin); 
- 
-// check if the pushbutton is pressed. 
-// if it is, the buttonState is HIGH: 
-if (buttonState == HIGH) {  
-// turn LED on:  
-digitalWrite(ledPin, HIGH);  
-}  
-else { 
-// turn LED off: 
-digitalWrite(ledPin, LOW);  
-} 
+#include <TinkerKit.h>
+
+TKButton button(I0);	// creating the object 'button' that belongs to the 'TKButton' class 
+                        // and giving the value to the desired input pin
+
+TKLed led(O0);		// creating the object 'led' that belongs to the 'TKLed' class 
+                        // and giving the value to the desired output pin
+
+
+void setup() {
+//nothing here
+}
+
+void loop()
+{
+  // check the switchState of the button
+  if (button.readSwitch() == HIGH) {	  
+    led.on();			 
+  } 
+  else {			
+    led.off();			
+  }
 }
