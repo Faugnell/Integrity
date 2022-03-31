cc2d8159735bf025d34c3df2e39920157d7c6427
Faugnell <90843661+Faugnell@users.noreply.github.com>
2022-03-31 14:50:55 +0200

    Update poc-rfid.ino

--- src/poc/poc-rfid.ino
+++ src/poc/poc-rfid.ino
@@ -1,55 +1,90 @@
-#include <SPI.h> // SPI
-#include <MFRC522.h> // RFID
-#include <string.h>
-#include <stdio.h>
+/*!
+* @file arduino.c
+* @author Victor Petit
+* @license GPL
+* @brief machine à état pour contrôler l'intégrité d'un sas antivirus
+*/
 
+#include <SPI.h>
+#include <MFRC522.h>
+#include <TinkerKit.h>
+
+// Déclaration des constantes
+#define LOOP_DELAY 1
+#define RFID_SIZE 20
 #define SS_PIN 10
 #define RST_PIN 9
+#define BADGE_ABSENT 1
+#define BADGE_ERROR 2
+#define BADGE_OK 0
 
-MFRC522 rfid(SS_PIN, RST_PIN); 
+MFRC522 rfid(SS_PIN, RST_PIN);
 byte nuidPICC[255];
 
-void setup() 
-{ 
-  Serial.begin(9600);
-  SPI.begin(); 
-  rfid.PCD_Init(); 
-}
- 
-int srfid(String *s) 
+// Déclaration des variables
+unsigned long InternalRefTime;
+int inputStatusReadRfid;
+String inputRfidBadge;
+String inputRfidBadgeUser;
+
+
+/*!
+* @brief détection de la présence et lecture du badge
+* @param s, pointeur sur une chaine de caractères
+* @return BADGE_OK, seulement si un badge est lu et enregistré
+* @return BADGE_ABSENT si aucun badge n'est détecté
+* @return BADGE_ERROR Si le contenu du badge n'a pas pu être lu
+*/
+int ReadRfid(String *s)
 {
   if ( !rfid.PICC_IsNewCardPresent())
-    return 1; 
+    return BADGE_ABSENT;
+ 
   if ( !rfid.PICC_ReadCardSerial())
-    return 2;
-  for (int i = 0; i < rfid.uid.size; i++) 
+    return BADGE_ERROR;
+ 
+  for (int i = 0; i < rfid.uid.size; i++)
   {
     nuidPICC[i] = rfid.uid.uidByte[i];
-    *s = *s + String(rfid.uid.uidByte[i],HEX);
+    *s = *s + String(rfid.uid.uidByte[i], HEX);
   }
+  
+  // Halt PICC
   rfid.PICC_HaltA();
+  
+  // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
-  return 0;
+  
+  return BADGE_OK;
 }
 
+/*!
+* @brief initialisation du moniteur et du lecteur RFID
+* @param void
+* @return void
+*/
+void setup()
+{
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.println("INIT");
+}
+
+/*!
+* @brief boucle de traitement qui prend en compte les entrés et sorties
+* @param void
+* @return void
+*/
 void loop()
 {
-  String p = "";
-  srfid(&p);
-  Serial.print(p);
-  if(p!="")
+  // Lecture des entrées
+  inputRfidBadge = "";
+  inputStatusReadRfid = ReadRfid(&inputRfidBadge);
+  if(inputStatusReadRfid == BADGE_OK)
   {
-    if (p== "432892f65e80")
-    {
-      Serial.println("");
-      Serial.print("oui");
-      Serial.println("");
-    }
-    else
-    {
-      Serial.println("");
-      Serial.print("non");
-      Serial.println("");
-    }
+     Serial.println("Lecture badge OK");
   }
+
+  delay(LOOP_DELAY);
 }
